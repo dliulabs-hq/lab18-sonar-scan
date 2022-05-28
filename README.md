4166a30bd6e52131df06a331200e23b185645a14
David Liu <david.s.liu@gmail.com>
2022-05-28 14:31:20 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# Weather Walking Skeleton
+
+[Testing an ASP.NET Core Service with xUnit](https://www.jeremywells.io/2020/08/03/weather-walking-skeleton-03.html)
+
+This is a repo for a series of blog posts that walkthroughj building a minimally functional web application using:
+
+- ASP.NET Core
+- Angular
+- Azure
+- (And maybe other technologies along the way).
+
+You can follow this series either on [JeremyWells.io](https://jeremywells.io), [dev.to](https://dev.to/jsheridanwells) or [Medium](https://medium.com/@jsheridanwells_66599)
+
+## SonarCloud Scanning
+
+[Sample SonarCloud Report](https://sonarcloud.io/summary/overall?id=lab18-sonar-scan)
+
+```
+brew install openjdk@11
+export PATH="/usr/local/opt/openjdk@11/bin:$PATH"
+echo 'export PATH="/usr/local/opt/openjdk@11/bin:$PATH"' >> ~/.zshrc
+. ~/.zshrc
+java --version
+dotnet tool install --global dotnet-sonarscanner --version 5.5.3
+```
+
+```
+export SONAR_TOKEN=<token>
+dotnet sonarscanner begin /o:dliu /k:lab18-sonar-scan /d:sonar.host.url=https://sonarcloud.io /v:"1.0" /d:sonar.login=$SONAR_TOKEN /d:sonar.cs.opencover.reportsPaths="**/coverage.opencover.xml" /d:sonar.coverage.exclusions="**Test*.cs" /d:sonar.exclusions="sonar-project.properties, .**/**, **/wwwroot/**, **/TestResults/**, **/obj/**, **/bin/**"
+dotnet build --no-incremental
+dotnet test /p:CollectCoverage=true /p:CoverletOutputFormat=opencover --collect:"XPlat Code Coverage"
+dotnet sonarscanner end /d:sonar.login=$SONAR_TOKEN
+```
---
+++ WeatherWalkingSkeleton/Services/OpenWeatherService.cs
@@ -0,0 +1,78 @@
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Options;
+using WeatherWalkingSkeleton.Config;
+using WeatherWalkingSkeleton.Infrastructure;
+using WeatherWalkingSkeleton.Models;
+
+namespace WeatherWalkingSkeleton.Services
+{
+    public enum Unit
+    {
+        Metric,
+        Imperial,
+        Kelvin
+    }
+
+    public interface IOpenWeatherService
+    {
+        Task<List<WeatherForecast>> GetFiveDayForecastAsync(string location, Unit unit = Unit.Metric);
+    }
+    
+    public class OpenWeatherService : IOpenWeatherService
+    {
+        private readonly OpenWeather _openWeatherConfig;
+        private readonly IHttpClientFactory _httpFactory;
+
+        public OpenWeatherService(IOptions<OpenWeather> opts, IHttpClientFactory httpFactory)
+        {
+            _openWeatherConfig = opts.Value;
+            _httpFactory = httpFactory;
+        }
+
+        public async Task<List<WeatherForecast>> GetFiveDayForecastAsync(string location, Unit unit = Unit.Metric)
+        {
+            string url = BuildOpenWeatherUrl("forecast", location, unit);
+            var forecasts = new List<WeatherForecast>();
+           
+            var client = _httpFactory.CreateClient("OpenWeatherClient");
+            var response = await client.GetAsync(url);
+            
+            if (response.IsSuccessStatusCode)
+            {
+                var jsonOpts = new JsonSerializerOptions {IgnoreNullValues = true, PropertyNameCaseInsensitive = true};
+                var contentStream = await response.Content.ReadAsStreamAsync();
+                var openWeatherResponse = await JsonSerializer.DeserializeAsync<OpenWeatherResponse>(contentStream, jsonOpts);
+                foreach (var forecast in openWeatherResponse.Forecasts)
+                {
+                    forecasts.Add(new WeatherForecast
+                    {
+                        Date = new DateTime(forecast.Dt),
+                        Temp = forecast.Temps.Temp,
+                        FeelsLike = forecast.Temps.FeelsLike,
+                        TempMin = forecast.Temps.TempMin,
+                        TempMax = forecast.Temps.TempMax,
+                    });
+                } 
+                
+                return forecasts;
+            }
+            else
+            {
+                throw new OpenWeatherException(response.StatusCode, "Error response from OpenWeatherApi: " + response.ReasonPhrase);
+            } 
+        }
+        
+        private string BuildOpenWeatherUrl(string resource, string location, Unit unit)
+        {
+            return $"https://api.openweathermap.org/data/2.5/{resource}" +
+                   $"?appid={_openWeatherConfig.ApiKey}" +
+                   $"&q={location}" +
+                   $"&units={unit}";
+        }
+    }
+}
